b479284b9b09fb6cdcb28aed57ca4f870c2acfb0
Pablo Guzman-Sanchez <p.guzman-sanchez@denso-adas.de>
2018-01-16 22:03:33 +0100

    Added new images for writeup

--- lane_line_detection.py
+++ lane_line_detection.py
@@ -78,53 +78,7 @@ def ipm_image_proc(image, roi):
 
     return warped, H, H_inv
 # ------------------------------------------------------------------------------------
-def window_mask(width, height, img_ref, center, level):
-    output = np.zeros_like(img_ref)
-    output[int(img_ref.shape[0] - (level + 1) * height):int(img_ref.shape[0] - level * height),
-    max(0, int(center - width / 2)):min(int(center + width / 2), img_ref.shape[1])] = 1
-    return output
-# ------------------------------------------------------------------------------------
-def find_window_centroids(image, window_width, window_height, margin):
-    window_centroids = []  # Store the (left,right) window centroid positions per level
-    window = np.ones(window_width)  # Create our window template that we will use for convolutions
-
-    # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
-    # and then np.convolve the vertical image slice with the window template
-
-    # Sum quarter bottom of image to get slice, could use a different ratio
-    l_sum = np.sum(image[int(3 * image.shape[0] / 4):, :int(image.shape[1] / 2)], axis=0)
-    l_center = np.argmax(np.convolve(window, l_sum)) - window_width / 2
-    r_sum = np.sum(image[int(3 * image.shape[0] / 4):, int(image.shape[1] / 2):], axis=0)
-    r_center = np.argmax(np.convolve(window, r_sum)) - window_width / 2 + int(image.shape[1] / 2)
-
-    # Add what we found for the first layer
-    window_centroids.append((l_center, r_center))
-
-    # Go through each layer looking for max pixel locations
-    for level in range(1, (int)(image.shape[0] / window_height)):
-        # convolve the window into the vertical slice of the image
-        image_layer = np.sum(
-            image[int(image.shape[0] - (level + 1) * window_height):int(image.shape[0] - level * window_height), :],
-            axis=0)
-        conv_signal = np.convolve(window, image_layer)
-        # Find the best left centroid by using past left center as a reference
-        # Use window_width/2 as offset because convolution signal reference is at right side of window, not center of window
-        offset = window_width / 2
-        l_min_index = int(max(l_center + offset - margin, 0))
-        l_max_index = int(min(l_center + offset + margin, image.shape[1]))
-        l_center = np.argmax(conv_signal[l_min_index:l_max_index]) + l_min_index - offset
-        # Find the best right centroid by using past right center as a reference
-        r_min_index = int(max(r_center + offset - margin, 0))
-        r_max_index = int(min(r_center + offset + margin, image.shape[1]))
-        r_center = np.argmax(conv_signal[r_min_index:r_max_index]) + r_min_index - offset
-        # Add what we found for that layer
-        window_centroids.append((l_center, r_center))
-
-    return window_centroids
-
-# ------------------------------------------------------------------------------------
 def detect_lines(image, nwindows=9, margin=80, minpix=50):
-
 
     # Make a binary and transform image
     binary_warped = image
@@ -245,23 +199,8 @@ def detect_lane(image, left_fit_temporal,right_fit_temporal,first_time):
        left_fit_temporal = left_fit_temporal * (1 - alfa) + left_fit * alfa
        right_fit_temporal = right_fit_temporal * (1 - alfa) + right_fit * alfa
 
-    # Visualization
-    #ploty = np.linspace(0, image.shape[0]-1, image.shape[0])
-    #left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-    #right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
-    #out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 100, 0]
-    #out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [255, 255, 0]
-    # plt.imshow(out_img)
-    # plt.plot(left_fitx, ploty, color='white')
-    # plt.plot(right_fitx, ploty, color='white')
-    # plt.pause(0.05)
-    # plt.clf()
-
-
-
     return left_fit_temporal, right_fit_temporal,left_curverad, right_curverad
-
+# --------------------------------------------------------------------------------
 def get_road_features(s_channel):
 
     thr = 35
@@ -326,92 +265,6 @@ def get_road_features(s_channel):
     # cv2.imshow("satur", binary_image)
     # cv2.waitKey(0)
     return binary_image
-# ------------------------------------------------------------------------------------
-def plot_lane_dots(binary, window_centroids, window_width, window_height):
-
-    ipm_img_color = cv2.cvtColor(binary, cv2.COLOR_GRAY2RGB)
-    color_l = [0, 255, 0]
-    color_r = [0, 0, 255]
-    if len(window_centroids) > 0:
-        for level in range(0, len(window_centroids)):
-            cv2.circle(ipm_img_color, (max(0, int(window_centroids[level][0] - (window_height / 2) + window_width/2)),
-                                       int(ipm_img_color.shape[0] - (level + 1) * window_height)), 8, color_l, -1)
-            cv2.circle(ipm_img_color, (max(0, int(window_centroids[level][1] - (window_height / 2) + window_width/2)),
-                                       int(ipm_img_color.shape[1] - (level + 1) * window_height)), 8, color_r, -1)
-
-        cv2.imshow("points", ipm_img_color)
-        cv2.waitKey(10)
-
-    # if len(window_centroids) > 0:
-    #
-    #     # Points used to draw all the left and right windows
-    #     l_points = np.zeros_like(ipm_img)
-    #     r_points = np.zeros_like(ipm_img)
-    #
-    #     # Go through each level and draw the windows
-    #     for level in range(0, len(window_centroids)):
-    #         # Window_mask is a function to draw window areas
-    #         l_mask = window_mask(window_width, window_height, ipm_img, window_centroids[level][0], level)
-    #         r_mask = window_mask(window_width, window_height, ipm_img, window_centroids[level][1], level)
-    #         # Add graphic points from window mask here to total pixels found
-    #         l_points[(l_points == 255) | ((l_mask == 1))] = 255
-    #         r_points[(r_points == 255) | ((r_mask == 1))] = 255
-    #
-    #     # Draw the results
-    #     template = np.array(r_points + l_points, np.uint8)  # add both left and right window pixels together
-    #     zero_channel = np.zeros_like(template)  # create a zero color channel
-    #     template = np.array(cv2.merge((zero_channel, template, zero_channel)), np.uint8)  # make window pixels green
-    #     warpage = np.dstack((ipm_img, ipm_img, ipm_img)) * 255  # making the original road pixels 3 color channels
-    #     output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0)  # overlay the orignal road image with window results
-
-    # If no window centers found, just display orginal road image
-    else:
-        output = np.array(cv2.merge((ipm_img, ipm_img, ipm_img)), np.uint8)
-
-    # Display the final results
-    # plt.imshow(output)
-    # plt.title('window fitting results')
-    # plt.show()
-# ------------------------------------------------------------------------------------
-def generate_polynom(road_features, window_centroids):
-
-    x_left = []
-    y_left = []
-    x_right = []
-    y_right = []
-    if len(window_centroids) > 0:
-       for level in range(0, len(window_centroids)):
-           y_left.append(max(0, int(window_centroids[level][0] - (window_height / 2) + window_width)))
-           x_left.append(int(road_features.shape[0] - (level + 1) * window_height))
-
-           y_right.append(max(0, int(window_centroids[level][1] - (window_height / 2) + window_width)))
-           x_right.append(int(road_features.shape[1] - (level + 1) * window_height))
-
-    left_fit = np.polyfit(x_left, y_left, 2)
-    right_fit = np.polyfit(x_right, y_right, 2)
-    x_left = np.array(x_left)
-    y_left = np.array(y_left)
-    x_right = np.array(x_right)
-    y_right = np.array(y_right)
-
-    left_fity = left_fit[0] * x_left ** 2 + left_fit[1] * x_left + left_fit[2]
-    right_fity = right_fit[0] * x_right ** 2 + right_fit[1] * x_right + right_fit[2]
-
-    road_features = cv2.cvtColor(road_features, cv2.COLOR_GRAY2RGB)
-    color_l = [0, 255, 0]
-    color_r = [0, 0, 255]
-
-    for x in range(x_left.shape[0]-1):
-        #cv2.line(road_features,(np.int32(x_left[x]),np.int32(left_fity[x])),(np.int32(x_left[x+1]),np.int32(left_fity[x+1])),color_l)
-        # Left
-        cv2.line(road_features,(np.int32(left_fity[x]),np.int32(x_left[x])),(np.int32(left_fity[x+1]),np.int32(x_left[x+1])),color_l)
-        # Right
-        cv2.line(road_features,(np.int32(right_fity[x]),np.int32(x_right[x])),(np.int32(right_fity[x+1]),np.int32(x_right[x+1])),color_r)
-
-    cv2.imshow("line", road_features)
-    cv2.waitKey(10)
-
-    return left_fit, right_fit, x_left, x_right
 # ------------------------------------------------------------------------------------
 # PARAMETERS
 # ------------------------------------------------------------------------------------
@@ -526,7 +379,7 @@ while(cap.isOpened()):
     # ------------------------------------------------------------------------------------
     # Detect lane
     # ------------------------------------------------------------------------------------
-    left_fit_temporal, right_fit_temporal = detect_lane(np.uint8(road_features_integral), left_fit_temporal,
+    left_fit_temporal, right_fit_temporal,left_curverad, right_curverad = detect_lane(np.uint8(road_features_integral), left_fit_temporal,
                                                         right_fit_temporal, first_time_line)
     first_time_line = False
     # ------------------------------------------------------------------------------------
